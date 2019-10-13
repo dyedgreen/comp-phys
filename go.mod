86cea83cee842466b7f44ce60f99d05a531783d7
Tilman Roeder <dyed.green.info@gmail.com>
2019-10-13 20:30:36 +0100

    Add problem 4 (1)

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dyedgreen/comp-phys
 
 go 1.13
+
+require gonum.org/v1/plot v0.0.0-20191004082913-159cd04f920c
---
+++ problems/ps-4/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"math"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/plotutil"
+	"gonum.org/v1/plot/vg"
+)
+
+const (
+	N     = 30
+	start = 0.0
+	end   = 20.0
+)
+
+func generateData() plotter.XYs {
+	data := make(plotter.XYs, N)
+	x := start
+	for i := 0; i < len(data); i++ {
+		data[i] = plotter.XY{x, math.J0(x)}
+		x += (end - start) / N
+	}
+	return data
+}
+
+func plotLines(title, file string, lines ...interface{}) error {
+	p, err := plot.New()
+	if err != nil {
+		return err
+	}
+	p.Title.Text = title
+	if err := plotutil.AddScatters(p, "data", lines[0]); err != nil {
+		return err
+	}
+	if err := plotutil.AddLines(p, "lagrange", lines[1]); err != nil {
+		return err
+	}
+	if err := p.Save(8*vg.Inch, 5*vg.Inch, file); err != nil {
+		return err
+	}
+	return nil
+}
+
+func shitLagrange(data plotter.XYs, x float64) float64 {
+	// a O(N^2) Lagrange implementation
+	var y float64
+	for i := range data {
+		var top float64 = 1
+		var bot float64 = 1
+		for j := range data {
+			if j == i {
+				continue
+			}
+			top *= (x - data[j].X)
+			bot *= (data[i].X - data[j].X)
+		}
+		y += top * data[i].Y / bot
+	}
+	return y
+}
+
+func main() {
+	// Plot the data
+	data := generateData()
+	interpolation := make(plotter.XYs, 100)
+	x := start
+	step := (end - start) / float64(len(interpolation))
+	for i := range interpolation {
+		interpolation[i] = plotter.XY{x, shitLagrange(data, x)}
+		x += step
+	}
+	plotLines("What the heck is a Bessel function???", "bessel.png", data, interpolation)
+}
